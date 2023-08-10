42485e0c0c8ab0d57bab73ec3139b16b77801f94
Martin Ssebakumba <108685258+ENG-MARTIN@users.noreply.github.com>
2023-08-10 23:26:24 +0300

    Add files via upload

---
+++ LISTS.py
@@ -0,0 +1,12 @@
+#CREATING A LIST: FROM the list class
+mylist=['martin','martha','micheal','mathew','michelle','marvin','malik','mutebi']
+print(mylist)
+
+#adding contents to the list
+#using "apppend": it adds the new item at the end
+mylist.append("racheal")
+print(mylist)
+
+#adding to a particular value by index
+mylist(2,"jimmy")
+print(mylist)
